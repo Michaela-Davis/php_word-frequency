e7263ea7bc95f8ee4eb495011d74a234958cdf95
Michaela-Davis <michaela.delight@gmail.com>
2017-02-17 16:42:29 -0800

    fail second test

--- tests/WordFreqTest.php
+++ tests/WordFreqTest.php
@@ -11,26 +11,26 @@ class RepeatCounterTest extends PHPUnit_Framework_TestCase
             $test_RepeatCounter = new RepeatCounter($sentence, $find);
 
         ///   Act   ///
-            $result = $test_RepeatCounter->countRepeats();
+            $result = $test_RepeatCounter->countRepeats($sentence, $find);
 
         ///   Assert   ///
             $this->assertEquals("1", $result);
         }
 
-        // function test_WordFreqChecker_multiple()
-        // ///   Arrange   ///
-        //     $sentence = "cat walks the cat";
-        //     $find = "cat";
-        //     $count = "2";
-        //     $test_WordFreq = new WordFreqTest($sentence, $find, $count);
-        //
-        // ///   Act   ///
-        //     $result = $test_WordFreq->WordFreqTest();
-        //
-        // ///   Assert   ///
-        //     $this->assertEquals("cat walks the cat", $count);
-        // }
-        //
+        function test_RepeatCounter_multiple()
+        {
+        ///   Arrange   ///
+            $sentence = "cat walks the cat";
+            $find = "cat";
+            $test_RepeatCounter = new RepeatCounter($sentence, $find);
+
+        ///   Act   ///
+            $result = $test_RepeatCounter->countRepeats($sentence, $find);
+
+        ///   Assert   ///
+            $this->assertEquals("2", $result);
+        }
+
         // function test_WordFreqChecker_case()
         // ///   Arrange   ///
         //     $sentence = "Cat walks the cat";

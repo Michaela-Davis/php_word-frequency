a32ec7202e307e47576d529a9a257ac74b8a6c01
Michaela-Davis <michaela.delight@gmail.com>
2017-02-22 18:23:50 -0800

    add passing test_RepeatCounter_none

--- tests/WordFreqTest.php
+++ tests/WordFreqTest.php
@@ -3,6 +3,20 @@ require_once __DIR__ . "/../src/RepeatCounter.php";
 
 class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
+        function test_RepeatCounter_none()
+        {
+        ///   Arrange   ///
+            $sentence = "Walk the cat";
+            $find = "Valen";
+            $test_RepeatCounter = new RepeatCounter($sentence, $find);
+
+        ///   Act   ///
+            $result = $test_RepeatCounter->countRepeats($sentence, $find);
+
+        ///   Assert   ///
+            $this->assertEquals("0", $result);
+        }
+
         function test_RepeatCounter_single()
         {
         ///   Arrange   ///
